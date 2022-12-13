424fa2f062f84b33513b673f60be42badc668a4e
Mohul Srivastava <60806949+mohuls44@users.noreply.github.com>
2022-12-12 17:56:27 -0700

    Add files via upload

---
+++ GameViewController.swift
@@ -0,0 +1,61 @@
+//
+//  GameViewController.swift
+//  Alien Splash
+//
+//  Created by Mohul Srivastava on 11/10/22.
+//
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+import AVFoundation
+
+class GameViewController: UIViewController {
+
+    var bgm = AVAudioPlayer()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let filePath = Bundle.main.path(forResource: "bgm", ofType: "wav")
+        let audioNSURL = NSURL(fileURLWithPath: filePath!)
+        
+        do { bgm = try AVAudioPlayer(contentsOf: audioNSURL as URL)}
+        catch { return print("ERROR: Can't find audio track for BGM!")}
+        
+        bgm.numberOfLoops = -1
+        bgm.play()
+        
+        if let view = self.view as! SKView? {
+            // Load the SKScene from 'GameScene.sks'
+            let scene = MainMenuScene(size: (CGSize(width: 1536, height: 2048)))
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = false
+            view.showsNodeCount = false
+        }
+    }
+
+    override var shouldAutorotate: Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
